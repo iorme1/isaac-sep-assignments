86f91b36bfe70d79c160cc2b42e6f3d02931e0f9
Isaac Orme <isorme1@gmail.com>
2017-12-06 15:44:17 -0800

    hashes-2: completed

--- 01-data-structures/05-hashes-part-2/open_addressing/open_addressing.rb
+++ 01-data-structures/05-hashes-part-2/open_addressing/open_addressing.rb
@@ -2,29 +2,74 @@ require_relative 'node'
 
 class OpenAddressing
   def initialize(size)
+    @items = Array.new(size)
   end
 
   def []=(key, value)
+    hash_item = Node.new(key,value)
+    index = index(key,size)
+
+    if @items[index] === nil
+      @items[index] = hash_item
+      return
+    end
+
+    new_index = next_open_index(index)
+    if new_index == -1
+      resize
+    else
+      @items[new_index] = hash_item
+    end
   end
 
+
   def [](key)
+    @items.each_with_index do |item,i|
+     if item && item.key === key
+       return item.value
+     end
+   end
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
+    key.sum % size
   end
 
   # Given an index, find the next open index in @items
   def next_open_index(index)
+    looped = index
+    if index === @items.size - 1
+      index = 0
+    else
+      index +=1
+    end
+
+    while @items[index] && index != looped
+      if index === (@items.size - 1)
+        index = 0
+      else
+        index += 1
+      end
+    end
+
+     return index === looped ? -1 : index
   end
 
   # Simple method to return the number of items in the hash
   def size
+    @items.size
   end
 
   # Resize the hash
   def resize
+    temp = @items
+    @items = Array.new(@items.size * 2)
+
+    temp.each_with_index do |item,i|
+      @items[index(item.key,size)] = item if item
+    end
   end
 end
--- 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
@@ -5,29 +5,101 @@ class SeparateChaining
 
   def initialize(size)
     @max_load_factor = 0.7
+    @items = Array.new(size)
   end
 
   def []=(key, value)
-  end
+    hash_item = Node.new(key,value)
+    index = index(key,size)
+
+    if @items[index]
+      @items[index].add_to_tail(hash_item)
+    else
+      @items[index] = LinkedList.new
+      @items[index].add_to_tail(hash_item)
+    end
+      resize if load_factor > @max_load_factor
+   end
+
 
   def [](key)
+    index = index(key,size)
+    temp = @items[index].head
+    if temp.key === key
+      return temp.value
+    else
+      while temp.key != key && temp.next
+        temp = temp.next
+      end
+    temp.value
+    end
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
+    key.sum % size
   end
 
   # Calculate the current load factor
   def load_factor
+    count = 0.0
+    @items.each_with_index do |item,i|
+      if @items[i] && @items[i].head
+        count += 1
+        temp = @items[i].head
+        while temp.next
+          count += 1
+          temp = temp.next
+        end
+      end
+    end
+    lf = (count/@items.size)
+    lf
   end
+
+
+
+  def print_state
+    @items.each_with_index do |item,i|
+      if @items[i]
+        puts "#{@items[i].head.value}'s Bucket:"
+        puts "--------------------"
+        puts "#{@items[i].print}"
+        puts "--End of Bucket----"
+      end
+    end
+    load_factor
+  end
+
+
+
+
 
   # Simple method to return the number of items in the hash
   def size
+    @items.size
   end
 
   # Resize the hash
   def resize
+    temp = @items
+    @items = Array.new(@items.size * 2)
+
+    temp.each_with_index do |item,i|
+      if temp[i]
+        link = temp[i].head.next if temp[i].head.next
+        index = index(temp[i].head.key, size)
+        @items[index] = LinkedList.new unless @items[index]
+        @items[index].add_to_tail(temp[i].head)
+        while link
+          index = index(link.key,size)
+          @items[index] = LinkedList.new unless @items[index]
+          @items[index].add_to_tail(link)
+          link = link.next
+        end
+      end
+    end
   end
 end
